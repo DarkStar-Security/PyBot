aaf60d12520ac30a0539a8a2246ed3a163315289
ghostahull <ghostahull@protonmail.com>
2017-04-27 20:37:31 +0100

    Update bot_class.py

--- core/bot_class.py
+++ core/bot_class.py
@@ -1,132 +1,159 @@
+
 #!usr/bin/env python3.6
 # -*- coding: utf-8 -*-
-from PyBot.core import log as log
-from PyBot.core import commands as comd
+
+import asyncio
 import socket
 import ssl
 import re
 
+from core.addons import Commands
+from core.log import Log
 
-#This is the main class. Everything passes through this!
-class bot:
-    def __init__(self, server, chan, ssl=True):
+loop = asyncio.get_event_loop()
+def exe(coro): return loop.run_until_complete(coro)
 
-        self.server = server
-        self.chan = chan
-        self.port = 6697
-        #name
-        self.name = "PyBot"
-        #nick
-        self.nick = "PyBot"
-        #host : put name before the '@'
-        self.host = "python_b0t@127.0.0.1"
-        #Dont touch this
+# This is the main class. Everything passes through this!
+class Bot:
+    def __init__(self, config, ssl_on=True):
+        self.config = {
+            "server": config["server"],
+            "chans": config["chans"],
+            "port": config["port"],
+            "name": "PyBot_beta",    # Name
+            "nick": "PyBot_beta",    # Nick
+            "host": "PyBot_beta@127.0.0.1" , # Put a name before the '@'
+            "pass": "PythonIsBadAssAsFuckYouMotherFucker"
+        }
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 0)
         self.sock.setblocking(True)
-        if ssl:
-            return self.sock = ssl.wrap_socket(self.sock)
-        else:
-            pass
-    #connect to the server...
-    def connect(self):
+
+        self.log = Log("logs.txt")
+
+        if ssl_on:
+            self.sock = ssl.wrap_socket(self.sock)
+
+    # Connect to the server...
+    async def connect(self):
         try:
-            log.log_file()
             print("Trying to connect to the server...")
-            self.sock.connect((self.server, self.port))
+            print("server: ", self.config["server"], " port: ", self.config["port"])
+            self.sock.connect((self.config["server"], self.config["port"]))
             print("We did it!")
         except Exception as e:
             print("There was an exception....")
             print("Please look into the log file...")
             print("Exception: ", e)
-            log.report_error(e)
+            self.log.report_error(e)
+
     # Kill the bot
-    def kill_self(self):
+    def kill(self):
         return self.sock.close()
-        
-    #send data to the server (in a special format -> not privmsg)
-    def send_data(self, data):
+    
+    async def mode_set(self):  
         try:
-            self.sock.send((data).encode('UTF-8'))
+            print("Setting user mode...")
+            self.sock.send(("MODE {0} +B\r\n".format(self.config['nick'])).encode('UTF-8'))        
         except Exception as e:
-            print("There was an error sending the data...")
-            log.report_error(e)
-    #send a privmsg to a room
-    def send_message(self, msg):
+            print("There was an error setting mode...")
+            self.log.report_error(e)
+
+    async def identify(self):
+        try:  
+            print("Identifying with nickserv...")
+            self.sock.send(("PRIVMSG NICKSERV IDENTIFY {0}\r\n".format(self.config["pass"]).encode('UTF-8')))
+        except Exception as e:
+            print("There was an error identifying...")
+            self.log.report_error(e)
+
+    #def register_nickserv(self):
+        #self.sock.send("MSG NICKSERV REGISTER {0} myfakeemail@email.org\r\n".format(self.config['pass']).encode('UTF-8'))
+
+    # Send data to the server (in a special format, not a private msg)
+    async def send_data(self, data):
         try:
-            msg = str(msg)
-            self.sock.send(("PRIVMSG {} :{}\r\n".format(self.chan, msg)).encode('UTF-8'))
+            self.sock.send(data.encode('UTF-8'))
         except Exception as e:
-            print("There was an error sending the message...")
-            log.report_error(e)
-    #join a specefic channel. By default will use the one created in the class. You can join an additional one
-    #bycalling the function with an argument
+            print("There was an error sending the data.\n {0}".format(e))
+            self.log.report_error(e)
+
+    # Send a privmsg to a channel
+    async def send_message(self, msg, chan):
+        try:
+            self.sock.send(("PRIVMSG {} :{}\r\n".format(chan, str(msg))).encode('UTF-8'))
+        except Exception as e:
+            print("There was an error sending the message.")
+            self.log.report_error(e)
+
+    """
+        if you want to join more then one channel, please make a list when
+        creating the bot containing the channels you would like the bot to join.
+        don't forget to put them in strings with a '#' at the start.
+    """
+
     def join(self):
         try:
-            if isinstance(self.chan, list) :
-                for x in self.chan:
-                    join = "JOIN :{0}\r\n".format(x)
-                    print(join)
-                    self.send_data(join)
-            else:
-                join = "JOIN :{0}\r\n".format(self.chan)
-                print(join)
-                self.send_data(join)
-
+            if isinstance(self.config["chans"], list):
+                for chan in self.config["chans"]:
+                    join = "JOIN :{0}\r\n".format(chan)
+                    exe(self.send_data(join))
         except Exception as e:
-            print("There was an exception..")
-            log.report_error(e)
-
-    def recv_data(self):
+            print("There was an exception joining channels.")
+            self.log.report_error(e)
+            
+    async def recv_data(self):
         try:
-            data = self.sock.recv(3000)
-            data = data.decode('UTF-8')
-            print("Data recived:\t", data)
+            data = self.sock.recv(4096).decode("UTF-8")
+            print("Data received:\t", data)
             if data.find('PING') != -1:
                 data = data.split()
-                self.sock.send(("PONG {} \r\n".format(data[1])).encode('UTF-8'))
+                self.sock.send(("PONG {}\r\n".format(data[1])).encode('UTF-8'))
+            elif not data:
+                self.kill()
+                self.log.log_write("Connection closed. Recived No data -> stopping to be sure we don't DoS server")
             else:
                 data = str(data)
                 return data
-       except(Exception) as e:
-           raise Exception("The following Exception occured.\n{0}".format(e))
-           
-    def send_user(self):
+        except Exception as e:
+            self.log.report_error(e)
+
+    async def send_user(self):
         try:
             print("Sending the user....")
-            usr = "USER {0} {1} {2} : I'm a bot!\r\n".format(self.name, self.host, self.nick)
-            self.send_data(usr)
+            usr = "USER {0} {1} {2} : I'm a bot!\r\n".format(
+                self.config["name"],
+                self.config["host"],
+                self.config["nick"]
+            )
+            exe(self.send_data(usr))
         except Exception as e:
             print("There was an error sending the user...")
-            log.report_error(e)
+            self.log.report_error(e)
 
-    def send_nick(self):
+    async def send_nick(self):
         try:
             print("Sending the nick...")
-            nick = "NICK {}\r\n".format(self.nick)
-            self.send_data(nick)
+            nick = "NICK {}\r\n".format(self.config["nick"])
+            exe( self.send_data(nick) )
             print(nick)
         except Exception as e:
             print("There was an error sending the nick...")
-            log.report_error(e)
-'''
-    def command(self, data_sent):
-        answer = comd.find_command(data_sent, self.chan, self.nick)
-        if answer is None:
-            pass
-        elif "op" == answer[0]:
-            self.send_data("MODE {} +o {}\r\n".format(self.chan, answer[1]))
-        elif "unop" == answer[0]:
-            self.send_data("MODE {} -o {}\r\n".format(self.chan, answer[1]))
-        elif "ban" in answer[0]:
-            self.send_data("KICK {} {} {}\r\n".format(self.chan, answer[1], "You're fucking gay."))
-            self.send_data("MODE {} +b {}!*@*\r\n".format(self.chan, answer[1]))
-        elif "!file" in answer:
-            file_name = re.findall(r"!file\s+([a-zA-Z0-9|x]*)", answer)
-            _file = open(file_name[0] + ".txt", "r")
-            for x in _file:
-                self.send_message(x)
-        else:
-            payload = str(answer)
-            self.send_message(payload)
-'''
+            self.log.report_error(e)
+
+    async def command(self, data_sent):
+        try:
+            match = re.search(r"(JOIN|PRIVMSG) (#(\w|-|_)+)", data_sent)
+            channel = [match.group(1), match.group(2)] if match else None
+
+            if channel[0] == "JOIN":
+                answer = Commands(self.config).join(data_sent)
+            else:
+                answer = Commands(self.config).exec_command(data_sent)
+
+            if answer and not "PING :" in data_sent:
+                exe( self.send_message(answer, channel[0]) )
+        except Exception as e:
+            print("There was an error sending the data")
+            self.log.report_error(e)
